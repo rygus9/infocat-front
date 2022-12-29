7437c4977e0fc55d22c32944ca795d9d346734eb
Cuzz <rygus9@naver.com>
2022-12-29 18:36:20 +0900

    feat: 마이페이지 UI 완료, 리펙토링은;;;

--- src/components/mypage/WrapLabelShort.tsx
+++ src/components/mypage/WrapLabelShort.tsx
@@ -3,16 +3,18 @@ import { ReactNode } from 'react';
 interface WrapLabelShortProps {
   children: ReactNode;
   label: string;
-  htmlFor: string;
+  htmlFor?: string;
+  errorMessage?: string;
 }
 
-export default function WrapLabelShort({ children, label, htmlFor }: WrapLabelShortProps) {
+export default function WrapLabelShort({ children, label, htmlFor, errorMessage }: WrapLabelShortProps) {
   return (
     <div className="flex w-96 items-center">
-      <label htmlFor={htmlFor} className="inline-block w-28 flex-shrink-0 text-gray">
+      <label htmlFor={htmlFor || ''} className="inline-block w-28 flex-shrink-0 text-gray">
         {label}
       </label>
       {children}
+      <div className="text-sm text-red-500">{errorMessage}</div>
     </div>
   );
 }
